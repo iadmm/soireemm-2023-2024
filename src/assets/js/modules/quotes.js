035d6620fcf826286eec18b0df4bfd016f8c6dc9
Jérôme Coupé <jercoupe@gmail.com>
2023-09-27 15:29:33 +0200

    quotes and footer

--- src/assets/js/modules/quotes.js
+++ src/assets/js/modules/quotes.js
@@ -1,15 +1,31 @@
 const QUOTES = [
   {
-    text: "C'est pas demain la veille que Blender remplacera Maya",
+    text: "C'est pas demain la veille que Blender remplacera Maya.",
     author: "Salvatore Bordonaro",
   },
   {
-    text: "Avec l'âge on grandit moins, sauf de la prostate",
+    text: "Avec l'âge on grandit moins, sauf de la prostate.",
     author: "Jérôme Coupé",
   },
   {
-    text: "Old age is not for Sissies",
-    author: "Beth Davis",
+    text: "Les rendus sont rarement un long fleuve tranquille.",
+    author: "Fabrice Laroche",
+  },
+  {
+    text: "Je suis sûr d'une chose: la télévision, c'est mort !",
+    author: "Christophe Pesesse",
+  },
+  {
+    text: "On ne peut jamais tout règler en post-production.",
+    author: "Caroline Voglaire",
+  },
+  {
+    text: "C'est décidé, je partirai dans une belle explosion.",
+    author: "Damien Welsh",
+  },
+  {
+    text: "Les ours, les ours ... et le cerf de nos Ardennes alors ?",
+    author: "Sylvia Minnaert",
   },
 ];
 
